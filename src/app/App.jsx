fb58191c2682917e53c48fe6ecff95c10358ce4d
Afonso-Front-End <afonsogithub@gmail.com>
2024-08-01 23:59:48 -0300

    rejouice-clone

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,25 @@
 import Header from "../components/header/Header";
 import Main from "../components/main/Main";
 import "./app.css"
+import gsap from "gsap"
+import ScrollTrigger from "gsap-trial/ScrollTrigger"
+import ScrollSmoother from "gsap-trial/ScrollSmoother"
+import { useRef } from "react";
+import { useLayoutEffect } from "react";
+
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
 const App = () => {
+    const containerRef = useRef(null)
+    useLayoutEffect(()=>{
+        const smoother = ScrollSmoother.create({
+            wrapper: containerRef.current,
+            content: "#content",
+            smooth: 1,
+            effects: true,
+        })
+    },[])
     return (
-        <div id="container">
+        <div id="container" ref={containerRef}>
             <div id="content">
                 <Header/>
                 <Main/>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -13,6 +13,12 @@ import logoCardRivia from "../../assets/Rivian.svg"
 import logoCardOura from "../../assets/logo-oura-card-2.svg"
 import logoCardMoxion from "../../assets/logo-moxion-card-3.svg"
 
+import image_slider_1 from "../../assets/slider/a19d8427-5008-4dce-afa4-5d66118a0463_d7a851a9d8e2eb36cf93ffe4a6fa3701.avif"
+import image_slider_2 from "../../assets/slider/36450525-8a76-4a44-b6d4-59c00ad0b50d_RJ-Home-OuraRing-x2.avif"
+import image_slider_3 from "../../assets/slider/d3289122-3f25-43f3-b140-11a342a484d7_Mask+group+(38).avif"
+import image_slider_4 from "../../assets/slider/8059f4af-8ba3-4e5f-a14b-ba5ed760963f_Mask+group+(39).avif"
+import image_slider_5 from "../../assets/slider/57ac2830-96e7-4174-a104-b384126bf98c_fec955552eb681ff6c273774515e6c2e.avif"
+
 const Main = () => {
     return (
         <div className="container-main">
@@ -106,6 +112,42 @@ const Main = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="container-section">
+                    <div className="content-section">
+
+                        <div className="title-slider">
+                            <p>Get in touch.</p>
+                            <a href="#"><span>↵</span>Contact</a>
+                        </div>
+                        <div className="container-slider-image">
+                            <div className="content-slider-image">
+                                <img src={image_slider_1} alt="" />
+                                <img src={image_slider_2} alt="" />
+                                <img src={image_slider_3} alt="" />
+                                <img src={image_slider_4} alt="" />
+                                <img src={image_slider_5} alt="" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="container-section">
+                    <div className="content-section">
+
+                    </div>
+                </div>
+
+                <div className="container-section">
+                    <div className="content-section">
+                        <div className="about">
+                            <p>We’re uniquely built↵</p>
+                            <h2><a href="#">About us</a></h2>
+                        </div>
+                    </div>
+                </div>
+
             </main>
         </div>
     )
